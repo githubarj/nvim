7083aa4768c6dc40f893764740111ccdf27b5010
Richard Githuba <githuba9520@gmail.com>
2024-09-30 08:42:35 +0300

    nvim

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,6 +3,13 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        custom_highlights = function(colors)
+          return {}
+        end,
+      })
+    end,
   },
 
   {
